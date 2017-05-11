97394f89d38651ecc2cbf14f3a60b3181d9da872
Jason <jasonkwong11@gmail.com>
2017-05-11 00:58:47 -0400

    set up boilerplate and eslint

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const title = "Redux SoundCloud: Good Morning Jason"
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot-loader!babel-loader'
+    }, 
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loaders: ['eslint-loader', 'babel-loader']
+    }]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + 'dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+};
