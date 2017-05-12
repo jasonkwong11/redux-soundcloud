2fbc8841f118cf8b8861c8912c34965f0fc47469
Jason <jasonkwong11@gmail.com>
2017-05-12 15:53:37 -0400

    wrote stream component and setup jest

---
+++ src/components/Stream.js
@@ -0,0 +1,11 @@
+function Stream({ tracks = [] }) {
+  return (
+    <div>
+      {
+        tracks.map((track, key) => <div className="track" key={key}>{track.title}</div>)
+      }
+    </div>
+  );
+}
+
+export default Stream;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 /* eslint-disable */
+'use strict';
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Stream from './components/Stream'
 
 const title = "Redux SoundCloud: Good Morning Jason"
+const tracks = [
+  {
+    title: 'Some track'
+  },
+  {
+    title: "Some other track"
+  }
+]
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <Stream tracks={tracks} />,
   document.getElementById('app')
 );
 
 module.hot.accept();
-
-/* eslint-disable */
