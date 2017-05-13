aef8e36698028ef589511971f07058f649627c93
Jason <jasonkwong11@gmail.com>
2017-05-12 21:27:04 -0400

    set up react router and react-router-redux

---
+++ src/components/App/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// use this component to implement static Header,
+// Footer, Playlist or Player components
+function App({ children }) {
+  return <div>{children}</div>;
+}
+
+export default App;
--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -1,10 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
+import { browserHistory } from 'react-router';
+import { routerMiddleware } from 'react-router-redux';
 import rootReducer from '../reducers/index';
 
 const logger = createLogger();
+// sync our store with the browser history, so we can
+// listen later on to events based on our current route
+// additionally, properties like browser path and 
+// query params in the URL can be accessed in the store
+const router = routerMiddleware(browserHistory);
 
-const createStoreWithMiddleWare = applyMiddleware(logger)(createStore);
+const createStoreWithMiddleWare = applyMiddleware(router, logger)(createStore);
 
 export default function configureStore(initialState) {
   return createStoreWithMiddleWare(rootReducer, initialState);
